dbda23c623810ae4d460dcf6ea4ea594f94f1797
Nat89 <grupaa.nowak@gmail.com>
2019-02-14 10:05:43 +0100

    START

---
+++ src/start/Hello.java
@@ -0,0 +1,11 @@
+package start;
+
+// Komentarz
+// psvm - public static void main (String[] args)
+// sout - System.out.println() ;
+// CTRL + SHIFT + F10 - run
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
