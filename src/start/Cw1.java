540fde202b3fe006f667ba194ff0e55b108fa988
Nat89 <grupaa.nowak@gmail.com>
2019-02-15 14:24:36 +0100

    14:24

---
+++ src/start/Cw1.java
@@ -0,0 +1,38 @@
+package start;
+
+import java.util.Scanner;
+
+public class Cw1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int key = -1;
+        int promien;
+
+        while (key != 0){
+            // pobierany promien
+            System.out.println("Wpisz wartość promienia: ");
+        promien = scanner.nextInt();
+
+        System.out.println("Wybierz funkcję do obliczeń: \n (1) Pole Koła \n (2) Obwód okręgu \n (3) Sfera.");
+
+        //klucz do instrukcji switch-case
+        key = scanner.nextInt();
+        switch (key) {
+            case 1:
+                System.out.printf("Pole: &.2f ", Math.PI * Math.pow(promien, 2));
+                break;
+            case 2:
+                System.out.printf("Obwód &.2f \n\n", Math.PI * 2 * promien);
+            case 3:
+                System.out.printf("Sfera: &.2f \n\n", 4 * Math.PI * Math.pow(promien, 3) / 3);
+                break;
+            case 0:
+                System.out.println("Wyjście\n");
+                break;
+            default:
+                System.out.println("Błedny wybór!\n");
+        }}
+    }
+    }
+
--- src/start/TableExample.java
+++ src/start/TableExample.java
@@ -4,6 +4,11 @@ public class TableExample {
     public static void main(String[] args) {
         double signal [] = {0.5, 0.11, 0.43, 0.67, 0.01, 0.11};
         // wypisanie
+        // inicjalizacja zmiennej pomocniczej; warunek zatrzymania pętli; krok)
+//        for(int i =0;) {
+
+
+//        }
         System.out.println(signal[0]);
         System.out.println(signal[1]);
         System.out.println(signal[2]);
