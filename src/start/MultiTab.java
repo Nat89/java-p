6b6e21c20eff42cef3cec937a105682f79c6c23d
Nat89 <grupaa.nowak@gmail.com>
2019-02-15 12:40:24 +0100

    12:40

---
+++ src/start/MultiTab.java
@@ -0,0 +1,29 @@
+package start;
+
+public class MultiTab {
+    public static void main(String[] args) {
+        char line [] = {' ',' ' ,  ' ' };
+
+        char kik [][] = {line, line, line };
+
+        char kik2 [][] = {{'_','_','_'}, {'_','_','_'}, {'_','_','_'}};
+
+        System.out.printf("%c %c %c\n", kik2[0][0], kik2 [0][1], kik2 [0][2]);
+        System.out.printf("%c %c %c\n", kik2[1][0], kik2 [1][1], kik2 [1][2]);
+        System.out.printf("%c %c %c\n", kik2[2][0], kik2 [2][1], kik2 [2][2]);
+
+        kik2[1][2] = 'x';
+        System.out.println();
+        System.out.printf("%c %c %c\n", kik2[0][0], kik2 [0][1], kik2 [0][2]);
+        System.out.printf("%c %c %c\n", kik2[1][0], kik2 [1][1], kik2 [1][2]);
+        System.out.printf("%c %c %c\n", kik2[2][0], kik2 [2][1], kik2 [2][2]);
+
+        kik2[2][0] = '0';
+        System.out.println();
+        System.out.printf("%c %c %c\n", kik2[0][0], kik2 [0][1], kik2 [0][2]);
+        System.out.printf("%c %c %c\n", kik2[1][0], kik2 [1][1], kik2 [1][2]);
+        System.out.printf("%c %c %c\n", kik2[2][0], kik2 [2][1], kik2 [2][2]);
+
+
+    }
+}
