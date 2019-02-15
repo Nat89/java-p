9c310c9e1f2aa6c4b387a793e309fe77d37bbcfe
Nat89 <grupaa.nowak@gmail.com>
2019-02-15 12:10:23 +0100

    12:10

--- src/start/Zad4.java
+++ src/start/Zad4.java
@@ -2,6 +2,25 @@ package start;
 
 public class Zad4 {
     public static void main(String[] args) {
-        
+        int decimalNum = 6;
+        switch (decimalNum) {
+            case 0:
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+                default:
+                System.out.println("Nie ma takiej liczby");
+        }
     }
 }
