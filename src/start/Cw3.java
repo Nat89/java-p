a40bb45d8f1614ad2efcc870e8f5164f89193d5b
Nat89 <grupaa.nowak@gmail.com>
2019-02-15 16:09:31 +0100

    16:09

--- src/start/Cw3.java
+++ src/start/Cw3.java
@@ -29,13 +29,25 @@ int suma_większe = 0;
         System.out.println(suma_większe - suma_mniejsze);
 
         int max_value=numbers[0];
+        int max_value2=numbers[0];
+        int index = 0;
         for(int i =0; i<numbers.length; i++) {
             if (numbers[i]>max_value) {
                 max_value = numbers[i];
+                index = i;
+            }
+        }
+
+        for(int i = 0; i<numbers.length; i++) {
+    if(numbers[i] > max_value2 && index != 1) {
+    max_value2 = numbers[i];
 
             }
         }
+
         System.out.println(max_value);
+        System.out.println(max_value2);
+        System.out.println(max_value*max_value2);
     }
 
 }
